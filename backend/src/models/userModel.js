ad37c3f5f6f5d44aaa4df42923e7db1adb2ff8f3
Tushar Sharma <tushar.sharma29sep@gmail.com>
2025-04-27 23:47:54 +0530

    added axios in login

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -5,7 +5,6 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    gender: { type: String, enum: ['male', 'female'], required: true }
 });
 
 export const User = mongoose.model('users', userSchema);
--- frontend/components/auth-provider.tsx
+++ frontend/components/auth-provider.tsx
@@ -3,6 +3,9 @@
 import type React from "react"
 
 import { createContext, useContext, useEffect, useState } from "react"
+import axios from "axios";
+
+const API_BASE_URL = process.env.API_BASE_URL;
 
 type User = {
   id: string
@@ -40,11 +43,14 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   }, [])
 
   const login = async (email: string, password: string) => {
-    // In a real app, this would make an API call to authenticate
-    // For demo purposes, we'll simulate a successful login with any credentials
     try {
-      // Simulate API call delay
-      await new Promise((resolve) => setTimeout(resolve, 1000))
+
+      const response = await axios.post(`${API_BASE_URL}/api/auth/sign-in`, { email, password });
+
+      if (response.data) {
+
+      }
+
 
       // Mock user data
       const userData = {
