dad5176b69c727825d4824cc013603cba7301328
Tushar Sharma <tushar.sharma29sep@gmail.com>
2025-05-06 22:53:07 +0530

    redundant socket setup code deleted

--- backend/src/controllers/roomController.js
+++ backend/src/controllers/roomController.js
@@ -51,44 +51,4 @@ export const deleteRoom = async (req, res) => {
     res.json({ message: 'Room deleted successfully' });
 }
 
-export const setupSocketIO = (io) => {
-    io.on("connection", (socket) => {
-        console.log("A user connected:", socket.id);
 
-        socket.on("joinRoom", async ({ roomId, user }) => {
-            let room = await Room.findOne({ code: roomId });
-
-            if (!room) {
-                room = new Room({ code: roomId, users: [user] });
-            } else {
-                if (!room.users.includes(user)) {
-                    room.users.push(user);
-                }
-            }
-            await room.save();
-
-            socket.join(roomId);
-            io.to(roomId).emit("roomUsers", room.users);
-        });
-
-        socket.on("sendMessage", ({ roomId, message }) => {
-            io.to(roomId).emit("receiveMessage", message);
-        });
-
-        socket.on("leaveRoom", async ({ roomId, user }) => {
-            const room = await Room.findOne({ code: roomId });
-
-            if (room) {
-                room.users = room.users.filter((u) => u !== user);
-                await room.save();
-
-                socket.leave(roomId);
-                io.to(roomId).emit("roomUsers", room.users);
-            }
-        });
-
-        socket.on("disconnect", () => {
-            console.log("User disconnected:", socket.id);
-        });
-    });
-};
