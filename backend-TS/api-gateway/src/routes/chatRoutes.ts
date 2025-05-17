86ae75259b81f971536358508fbbc202075c8bd7
Tushar Sharma <tushar.sharma29sep@gmail.com>
2025-05-17 17:41:23 +0530

    created roomServiceController.ts in API-GATEWAY

--- backend-TS/api-gateway/src/routes/chatRoutes.ts
+++ backend-TS/api-gateway/src/routes/chatRoutes.ts
@@ -1,32 +1,11 @@
 import express from "express";
-import {
-    addAdmin,
-    addUser, createRoom,
-    getChat, getRooms,
-    removeAdmin,
-    removeUser,
-    sendMessageToChatService
-} from "../controllers/chatServiceController";
-import {authenticateToken} from "../middleware/authMiddleware";
+import { createRoom, getRooms } from "../controllers/roomServiceController";
+import { authenticateToken } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
-router.post("/send",authenticateToken,sendMessageToChatService);
-
 router.post("/create-room",authenticateToken,createRoom);
-
-router.get("/get-chat",authenticateToken,getChat);
 
 router.get("/get-rooms",authenticateToken,getRooms);
 
-router.put("/add-user",authenticateToken,addUser);
-
-router.delete("/remove-user",authenticateToken,removeUser);
-
-router.put("/add-admin",authenticateToken,addAdmin);
-
-router.delete("/remove-admin",authenticateToken,removeAdmin);
-
 export default router;
-
-
