b36e671c9d186f9b882c782bf13381a6b9136cc3
Tushar Sharma <tushar.sharma29sep@gmail.com>
2025-07-26 01:07:48 +0530

    changePassword controller changed

--- backend/auth-service/src/controllers/authController.ts
+++ backend/auth-service/src/controllers/authController.ts
@@ -1,4 +1,4 @@
-import  { Request, Response, NextFunction } from 'express';
+import {Request, Response, NextFunction} from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import {User} from '../models/userModel';
@@ -8,18 +8,18 @@ import {publishToQueue} from "../config/rabbitmq";
 import {getRedisClient} from '../config/redis';
 
 const env = process.env.NODE_ENV;
-dotenv.config({ path: `.env.${env}` });
+dotenv.config({path: `.env.${env}`});
 
 export const signUp = async (req: Request, res: Response, next: NextFunction) => {
     const {name, email, password} = req.body;
 
     // Check if all required fields are present
     if (!name || !email || !password) {
-        return res.status(400).json({ message: 'email and password are required.' });
+        return res.status(400).json({message: 'email and password are required.'});
     }
 
     try {
-        const existingUser = await User.findOne({ email });
+        const existingUser = await User.findOne({email});
         if (existingUser) {
             return res.status(400).send('User already exists.');
         }
@@ -28,21 +28,24 @@ export const signUp = async (req: Request, res: Response, next: NextFunction) =>
         const otp = await sendOTP(email);
 
         // Store user temporarily in Redis using the OTP token as key
-        const tempUserData = JSON.stringify({ name, password });
+        const tempUserData = JSON.stringify({name, password});
         const redisClient = getRedisClient();
         await redisClient.setEx(`otp:${email}`, 300, otp);
         await redisClient.setEx(`signup:${email}`, 300, tempUserData); // Expires in 5 minutes
 
-        return res.status(200).json({success: true, message: 'OTP sent to your email. Please enter the OTP to complete sign-up.' });
+        return res.status(200).json({
+            success: true,
+            message: 'OTP sent to your email. Please enter the OTP to complete sign-up.'
+        });
     } catch (err) {
         next(err);
     }
 };
 
 export const completeSignUp = async (req: Request, res: Response, next: NextFunction) => {
-    const { email, otp } = req.body;
+    const {email, otp} = req.body;
     if (!email || !otp) {
-        res.status(400).json({ message: 'Email and OTP are required.' });
+        res.status(400).json({message: 'Email and OTP are required.'});
         return;
     }
 
@@ -54,34 +57,33 @@ export const completeSignUp = async (req: Request, res: Response, next: NextFunc
 
             const userDataStr = await redisClient.get(`signup:${email}`);
             if (!userDataStr) {
-                return res.status(400).json({ message: 'No user data found or token expired.' });
+                return res.status(400).json({message: 'No user data found or token expired.'});
             }
 
-            const { name, password } = JSON.parse(userDataStr);
+            const {name, password} = JSON.parse(userDataStr);
 
             // Generate JWT token
-            const token = jwt.sign({ email: email }, process.env.JWT_SECRET as string, { expiresIn: "1d" });
+            const token = jwt.sign({email: email}, process.env.JWT_SECRET as string, {expiresIn: "1d"});
 
-            await publishToQueue("authQueue", { name, email, password });
+            await publishToQueue("authQueue", {name, email, password});
 
             await redisClient.del([`signup:${email}`, `otp:${email}`]); // Clean up Redis entry
 
             // Respond with token and success message
-            res.status(201).json({success: true, token, message: 'User signed up successfully.' });
+            res.status(201).json({success: true, token, message: 'User signed up successfully.'});
         } else {
             // OTP verification failed
-            res.status(400).json({ message: 'Invalid or expired otp' });
+            res.status(400).json({message: 'Invalid or expired otp'});
         }
     } catch (err) {
         next(err);
     }
 };
 
-
 export const signIn = async (req: Request, res: Response, next: NextFunction) => {
-    const { email, password } = req.body;
+    const {email, password} = req.body;
     try {
-        const user = await User.findOne({ email });
+        const user = await User.findOne({email});
         if (!user) {
             res.status(400).send('User does not exist.');
             return;
@@ -93,9 +95,9 @@ export const signIn = async (req: Request, res: Response, next: NextFunction) =>
             return;
         }
 
-        const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET as string, { expiresIn: '1d' });
+        const token = jwt.sign({email: user.email}, process.env.JWT_SECRET as string, {expiresIn: '1d'});
 
-        res.json({success: true, token, name:user.name });
+        res.json({success: true, token, name: user.name});
         return;
     } catch (err) {
         next(err);
@@ -103,46 +105,53 @@ export const signIn = async (req: Request, res: Response, next: NextFunction) =>
 };
 
 export const passwordReset = async (req: Request, res: Response, next: NextFunction) => {
-   try{
+    const {email} = req.body;
+    try {
 
-    const { email } = req.body;
+        const user = await User.find({email: email});
+        if (!user) {
+            res.status(400).json({msg: 'User does not exist.'});
+        }
 
-    const user = await User.find({email : email});
-    if (!user) {
-        res.status(400).json({msg :'User does not exist.'});
+        const otp = await passwordResetMail(email);
+        const redisClient = getRedisClient();
+        await redisClient.setEx(`otp:${email}`, 300, otp);
+
+        res.status(201).json({success: true, msg: 'Otp verification mail sent.'});
+
+    } catch (err) {
+        next(err);
     }
-
-    const otp = await passwordResetMail(email);
-    const redisClient = getRedisClient();
-    await redisClient.setEx(`otp:${email}`, 300, otp);
-
-    res.status(201).json({success:true, msg: 'Otp verification mail sent.'});
-
-   } catch (err) {
-       next(err);
-   }
 }
 
 export const changePassword = async (req: Request, res: Response, next: NextFunction) => {
+    const {email, password, otp} = req.body;
+    if (!email || !password || !otp) {
+        res.status(400).send('All fields are required.');
+    }
+
     try {
-        const { email, password, otp, otpToken } = req.body;
-        if(!email || !password || !otp || !otpToken)  {
-            res.status(400).send('All fields are required.');
+        const redisClient = getRedisClient();
+        const savedOtp = await redisClient.get(`otp:${email}`);
+
+        if (savedOtp && otp === savedOtp) {
+            const user = await User.find({email: email});
+            if (!user) {
+                res.status(400).json({msg: 'User does not exist.'});
+            }
+
+            const hashedPassword = await bcrypt.hash(password, 10);
+            if (!hashedPassword) {
+                res.status(400).send('Invalid password');
+            }
+
+            await User.updateOne({email: email}, {password: hashedPassword});
+            await redisClient.del(`otp:${email}`);
+            
+            res.status(200).json({success: true, msg: 'Password updated'});
+        } else {
+            res.status(400).send('Invalid or Expired OTP');
         }
-        const user = await User.find({email : email});
-        if (!user) {
-            res.status(400).json({msg :'User does not exist.'});
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-        if (!hashedPassword) {
-            res.status(400).send('Invalid password');
-        }
-
-        await User.updateOne({ email: email }, { password: hashedPassword });
-
-        res.status(200).json({success: true, msg: 'Password updated'});
-
     } catch (err) {
         next(err);
     }
