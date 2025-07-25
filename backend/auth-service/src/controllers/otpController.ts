bc59766e278414b4a8053755a3b3d58550b79d9a
Tushar Sharma <tushar.sharma29sep@gmail.com>
2025-07-26 00:57:04 +0530

    password reset mail controller changed

--- backend/auth-service/src/controllers/otpController.ts
+++ backend/auth-service/src/controllers/otpController.ts
@@ -1,5 +1,4 @@
 import transporter from '../config/nodemailerConfig';
-import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
 const env = process.env.NODE_ENV;
@@ -20,28 +19,8 @@ export const sendOTP = async (email: string) => {
     return otp;
 };
 
-// Verify OTP
-export const verifyOTP = async (token: string, otp: string) => {
-    try {
-        if (!token || !otp) {
-            return { success: false, message: 'Token and OTP are required' };
-        }
-
-        if (decoded.otp === otp) {
-            return { success: true, message: 'OTP verified successfully' };
-        } else {
-            return { success: false, message: 'Invalid OTP' };
-        }
-    } catch (error: unknown) {
-        return { success: false, message: 'Invalid or expired token' };
-    }
-};
-
 export const passwordResetMail = async (email: string) => {
     const otp = Math.floor(100000 + Math.random() * 900000).toString();
-
-    // Create a JWT containing the OTP and set its expiry
-    const token = jwt.sign({ otp }, process.env.JWT_SECRET as string, { expiresIn: '5m' });
 
     // Send OTP via email
     await transporter.sendMail({
@@ -51,5 +30,5 @@ export const passwordResetMail = async (email: string) => {
         text: `Your OTP code is ${otp}. It will expire in 5 minutes.`,
     });
 
-    return token;
+    return otp;
 };
