cfa4cfce19411f0199d885cb45344a6c41c64751
Tushar Sharma <tushar.sharma29sep@gmail.com>
2025-08-12 00:24:07 +0530

    modified user model

--- backend/auth-service/src/models/userModel.ts
+++ backend/auth-service/src/models/userModel.ts
@@ -3,16 +3,20 @@ import * as mongoose from "mongoose";
 export interface IUser extends Document{
     name: string;
     email:string;
-    password: string;
     rooms?: mongoose.Types.ObjectId[];
     videos?: mongoose.Types.ObjectId[];
+    picture?: string;
+    password?: string | null;
+    authProvider?: string;
 }
 const UserSchema = new mongoose.Schema<IUser>({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
     rooms:{type:[mongoose.Types.ObjectId]},
-    videos:{type:[mongoose.Types.ObjectId]}
+    videos:{type:[mongoose.Types.ObjectId]},
+    picture: String,
+    password: { type: String, default: null },
+    authProvider: { type: String, default: "local" },
 })
 
 export const User = mongoose.model<IUser>('User',UserSchema);
